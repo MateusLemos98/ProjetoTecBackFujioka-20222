4462051fbcaf88fc570b4fd9b03b8752c982f648
MateusLemos98 <mateuslsdm98@gmail.com>
2023-03-21 20:43:34 -0300

    Arquivos

--- src/main/java/br/com/fujideia/iesp/tecback/web/FilmeController.java
+++ src/main/java/br/com/fujideia/iesp/tecback/web/FilmeController.java
@@ -1,4 +1,17 @@
 package br.com.fujideia.iesp.tecback.web;
 
+import br.com.fujideia.iesp.tecback.model.Filme;
+import br.com.fujideia.iesp.tecback.service.FilmeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
 public class FilmeController {
+
+    @Autowired
+    private FilmeService service;
+
+    public ResponseEntity<Filme> salvar(Filme filme){
+        filme = service.salvar(filme);
+        return ResponseEntity.ok(filme);
+    }
 }
